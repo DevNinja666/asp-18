b41d34fa33b039e51c88c7fff899c21dc886e91a
Elnur <elnurgasanovv07@gmail.com>
2026-03-31 01:45:54 +0400

    Delete Auotorization directory

--- Auotorization/ProjectOwnerOrAdminRequirement.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Authorization;
-
-namespace ASP_16._TaskFlow_Ownership.Authorization;
-
-public class ProjectOwnerOrAdminRequirement
-    : IAuthorizationRequirement
-{
-}
--- Auotorization/TaskStatusChangeRequrement.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.AspNetCore.Authorization;
-
-namespace ASP_16._TaskFlow_Ownership.Authorization;
-
-public class TaskStatusChangeRequrement:IAuthorizationRequirement
-{
-}
